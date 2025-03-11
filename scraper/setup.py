cab7c6e62ae0d3dfbab5caa799c581259fefcb1f
nhatrang <nhatrang10a4.com>
2025-03-11 18:42:41 +0700

    end of the day

--- scraper/setup.py
+++ scraper/setup.py
@@ -1,25 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='finance_data_scraper',
-    version='0.1.0',
-    packages=find_packages(where='src'), 
-    package_dir={'': 'src'}, 
+    name="finance_data_scraper",
+    version="0.1.0",
+    packages=find_packages(),
     install_requires=[
-        'beautifulsoup4==4.9.3',
-        'selenium',
-        'webdriver-manager',
-        'cassandra-driver',
-        'requests',
-        'pandas',
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'scikit-learn',
-        'tensorflow',
-        'keras',
-        'torch',
-        'torchvision',
-        'fastai'
-    ]
+        "selenium",
+        "webdriver-manager",
+    ],
+    entry_points={
+        "console_scripts": [
+            "data_coverage_scraper=src.fetchers.tradingview.data_coverage_scraper:countries_scraper",
+        ],
+    },
 )
